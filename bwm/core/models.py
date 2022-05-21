c64d0372a83ce6e870c47e0995bedf34149de7b3
terrence <terrluo@qq.com>
2022-05-21 15:34:38 +0800

    格式化

--- bwm/core/models.py
+++ bwm/core/models.py
@@ -12,6 +12,9 @@ class IsType:
 class BaseModel(db.Model):
     __abstract__ = True
 
+    class IsDelete(IsType):
+        """是否删除"""
+
     id = sa.Column(sa.Integer, primary_key=True)
     create_time = sa.Column(
         sa.TIMESTAMP,
@@ -25,7 +28,6 @@ class BaseModel(db.Model):
         server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"),
         comment="更新时间",
     )
-    is_delete = sa.Column(sa.Boolean, nullable=False, default=False, comment="是否删除")
-
-    class IsDelete(IsType):
-        """是否删除"""
+    is_delete = sa.Column(
+        sa.Boolean, nullable=False, default=IsDelete.NO, comment="是否删除"
+    )
