84be52da96228e5418afeb704d254511c2a150ad
terrence <terrluo@qq.com>
2022-06-03 15:23:39 +0800

    时间处理修改

--- bwm/util/dt.py
+++ bwm/util/dt.py
@@ -1,23 +1,25 @@
+import typing as t
 from datetime import datetime
 
 import pytz
 from flask import current_app, g
 
+TIMEZONE = t.Union[pytz.BaseTzInfo, pytz.tzinfo.StaticTzInfo, pytz.tzinfo.DstTzInfo]
+
 
 def to_local(dt: datetime):
     if dt.tzinfo is None:
-        dt = dt.replace(tzinfo=pytz.utc)
-    local_timezone = get_timezone()
-    return dt.astimezone(local_timezone)
+        dt = pytz.utc.localize(dt)
+    return dt.astimezone(get_timezone())
 
 
 def to_utc(dt: datetime):
     if dt.tzinfo is None:
-        return dt.replace(tzinfo=pytz.utc)
+        dt = get_timezone().localize(dt)
     return dt.astimezone(pytz.utc)
 
 
-def get_timezone():
+def get_timezone() -> TIMEZONE:
     timezone = getattr(g, "timezone", None)
     if timezone is None:
         g.timezone = timezone = current_app.config["LOCAL_TIMEZONE"]
