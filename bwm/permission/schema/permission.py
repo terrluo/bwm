d92e6e843b4e61abba10a667f9da93ba57780fd5
terr <terrluo@qq.com>
2022-06-07 20:21:15 +0800

    通过角色id获取权限

--- bwm/permission/schema/permission.py
+++ bwm/permission/schema/permission.py
@@ -4,10 +4,29 @@ from bwm.model import menu, permission
 from bwm.permission.error.permission import PermissionError
 from bwm.util.component import get_db
 
+_db = get_db()
+
+
+def _validate_menu(menu_id):
+    if not _db.session.query(menu.Menu.query.filter_by(id=menu_id).exists()).scalar():
+        raise PermissionError.MENU_NOT_FOUND
+
+
+def _validate_role(role_id):
+    if not _db.session.query(
+        permission.Role.query.filter_by(id=role_id).exists()
+    ).scalar():
+        raise PermissionError.ROLE_NOT_FOUND
+
+
+class GetPermission(Schema):
+    role_id = fields.Int(required=True, validate=[_validate_role])
+    menu_id = fields.Int(messing=None, validate=[_validate_menu])
+
 
 class AddPermission(Schema):
-    role_id = fields.Int(required=True)
-    menu_id = fields.Int(required=True)
+    role_id = fields.Int(required=True, validate=[_validate_role])
+    menu_id = fields.Int(required=True, validate=[_validate_menu])
     is_visible = fields.Bool(required=True)
     is_operate = fields.Bool(required=True)
 
@@ -16,19 +35,7 @@ class AddPermission(Schema):
         role_id = data["role_id"]
         menu_id = data["menu_id"]
 
-        db = get_db()
-
-        if not db.session.query(
-            permission.Role.query.filter_by(id=role_id).exists()
-        ).scalar():
-            raise PermissionError.ROLE_NOT_FOUND
-
-        if not db.session.query(
-            menu.Menu.query.filter_by(id=menu_id).exists()
-        ).scalar():
-            raise PermissionError.MENU_NOT_FOUND
-
-        if db.session.query(
+        if _db.session.query(
             permission.Permission.query.filter_by(
                 role_id=role_id, menu_id=menu_id
             ).exists()
--- bwm/permission/service/permission.py
+++ bwm/permission/service/permission.py
@@ -1,7 +1,10 @@
+import typing as t
+
+from bwm.constants import CacheKey
 from bwm.core.schema import load_schema
 from bwm.core.service import CacheService
 from bwm.model import permission
-from bwm.permission.schema.permission import AddPermission
+from bwm.permission.schema.permission import AddPermission, GetPermission
 from bwm.type import ServiceData
 
 
@@ -19,3 +22,21 @@ class PermissionService(CacheService):
         self.db.session.add(_permission)
         self.db.session.commit()
         return _permission
+
+    @load_schema(GetPermission())
+    def get_permission(self, data: ServiceData, timeout=60 * 60 * 24):
+        role_id = data["role_id"]
+        menu_id = data["menu_id"]
+        key = CacheKey.permission(role_id)
+        all_permission = self.cache.get(key)
+        if all_permission is None:
+            permission_list: t.List[permission.Permission] = self.model.query.filter_by(
+                role_id=role_id, is_delete=self.model.IsDelete.NO
+            )
+            all_permission = {perm.menu_id: perm for perm in permission_list}
+            self.cache.set(key, all_permission, timeout)
+
+        if menu_id is not None:
+            return all_permission.get(menu_id, {})
+
+        return all_permission
