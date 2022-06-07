769c9de266ae7213a775e7a6a8ed239cacce0068
terr <terrluo@qq.com>
2022-06-07 20:20:47 +0800

    整合缓存key

--- bwm/constants.py
+++ bwm/constants.py
@@ -4,3 +4,13 @@ from enum import Enum
 class Env(str, Enum):
     DEV = "development"
     PROD = "production"
+
+
+class CacheKey:
+    @classmethod
+    def menu(cls):
+        return "menu"
+
+    @classmethod
+    def permission(cls, role_id: int):
+        return f"permission_{role_id}"
--- bwm/menu/service/menu.py
+++ bwm/menu/service/menu.py
@@ -1,5 +1,6 @@
 import typing as t
 
+from bwm.constants import CacheKey
 from bwm.core.schema import load_schema
 from bwm.core.service import CacheService
 from bwm.menu.schema import AddMenuSchema
@@ -11,13 +12,14 @@ class MenuService(CacheService):
     menu_model = menu.Menu
 
     def get_all_menu(self, timeout=60 * 60 * 24) -> t.List[menu.Menu]:
-        all_menu = self.cache.get("all_menu")
-        if not all_menu:
+        key = CacheKey.menu()
+        all_menu = self.cache.get(key)
+        if all_menu is None:
             all_menu = self.menu_model.query.filter(
                 self.menu_model.is_delete == self.menu_model.IsDelete.NO
             ).all()
             all_menu = {menu.id: menu for menu in all_menu}
-            self.cache.set("all_menu", all_menu, timeout)
+            self.cache.set(key, all_menu, timeout)
         return all_menu
 
     @load_schema(AddMenuSchema())
