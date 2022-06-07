fe2afbb5095bb518fd27fe2f31d445ce9f7aaf79
terr <terrluo@qq.com>
2022-06-07 14:56:08 +0800

    用户信息移除 union_id

--- bwm/account/api/user.py
+++ bwm/account/api/user.py
@@ -11,7 +11,6 @@ user_bp, user_api = create_route("user", __name__, url_prefix="/api/user")
 
 
 user_info = {
-    "union_id": fields.String(),
     "nickname": fields.String(),
     "username": fields.String(),
     "create_time": fields.DateTime(dt_format="iso8601", attribute="local_create_time"),
