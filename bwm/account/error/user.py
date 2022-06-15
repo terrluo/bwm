77b6c7e3ca0cb37d1d1c6786e96708f7e1864668
terr <terrluo@qq.com>
2022-06-15 10:45:24 +0800

    修改密码

--- bwm/account/error/user.py
+++ bwm/account/error/user.py
@@ -8,3 +8,5 @@ class UserError:
 
     NOT_FOUND = Error(40000, _("用户不存在"))
     PERMISSION_DENIED = Error(40001, _("没有权限"))
+    OLD_PASSWORD_ERROR = Error(40002, _("旧密码错误"))
+    NEW_PASSWORD_ERROR = Error(40003, _("新密码不一致"))
---
+++ bwm/account/schema/user.py
@@ -0,0 +1,40 @@
+from typing import TYPE_CHECKING
+
+from flask_jwt_extended import current_user
+from marshmallow import Schema, fields, validates_schema
+
+from bwm.account.error.user import UserError
+from bwm.util.component import get_db
+
+if TYPE_CHECKING:
+    from bwm.model import account
+
+    _User = account.User
+
+
+class ChangePasswordSchema(Schema):
+    old_password = fields.String(required=True, allow_none=False)
+    new_password = fields.String(required=True, allow_none=False)
+    new_password_check = fields.String(required=True, allow_none=False)
+
+    @validates_schema(skip_on_field_errors=True)
+    def validate_schema(self, data: dict, **kwargs):
+        old_password = data["old_password"]
+        new_password = data["new_password"]
+        new_password_check = data["new_password_check"]
+        user = self._get_user(data, **kwargs)
+
+        if not user.check_password(old_password):
+            raise UserError.OLD_PASSWORD_ERROR
+
+        if new_password != new_password_check:
+            raise UserError.NEW_PASSWORD_ERROR
+
+    def _get_user(self, data: dict) -> _User:
+        raise NotImplementedError()
+
+
+class ChangeOwnPasswordSchema(ChangePasswordSchema):
+    def _get_user(self, data: dict) -> _User:
+        db = get_db()
+        return db.session.merge(current_user)
